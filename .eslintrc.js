2795d2fafefd40b1b3151bb01ce0bb9eaab2a8b9
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-03-09 23:52:21 +0200

    implemented SwapiService class for fetching data from swapi

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "amd": true,
+    "node": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "indent": ["error", 2, { "ignoredNodes": ["TemplateLiteral"] }],
+    "no-unused-vars": "off",
+    "no-undef": "off"
+  }
 };
