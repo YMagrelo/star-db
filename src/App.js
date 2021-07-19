24985f1d2d75c1bbf8c48966bf22e035e7f0bee7
Yaroslav Magrelo <slava.magrelo@gmail.com>
2021-07-19 13:36:26 +0300

    add Login and Secret pages

--- src/App.js
+++ src/App.js
@@ -10,11 +10,26 @@ import PeoplePage from "./components/pages/people-page";
 import PlanetsPage from "./components/pages/planets-page";
 import StarshipsPage from "./components/pages/starships-page";
 import StarshipDetails from "./components/sw-components/starship-details";
+import LoginPage from "./components/pages/login-page";
+import SectretPage from "./components/pages/secret-page";
 
 export default class App extends React.Component {
   swapiService = new SwapiService();
 
+  state = {
+    isLoggedIn: false,
+  }
+
+  onLogin = () => {
+
+
+    return this.setState({
+      isLoggedIn: true,
+    })
+  }
+
   render() {
+    const {isLoggedIn} = this.state;
     return (
       <SwapiServiceProvider value={this.swapiService}>
         <Router>
@@ -42,6 +57,12 @@ export default class App extends React.Component {
               return <StarshipDetails itemId={id} />
             }}
           />
+          <Route path="/login" render={() => (
+            <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin}/>
+          )} />
+          <Route path ="/secret" render={() => (
+            <SectretPage isLoggedIn={isLoggedIn} />
+          )} />
 
         </div>
         </Router>
